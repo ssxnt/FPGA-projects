9a904374b34fe2d81c434e3047697b343e87493f
Sant <ssants21cf@gmail.com>
2023-09-17 18:40:56 -0700

    lab 2 init

---
+++ Lab-2/Lab-1/task1/tb_card7seg.sv
@@ -0,0 +1,8 @@
+module tb_card7seg();
+
+// Your testbench goes here. Make sure your tests exercise the entire design
+// in the .sv file.  Note that in our tests the simulator will exit after
+// 10,000 ticks (equivalent to "initial #10000 $finish();").
+						
+endmodule
+
