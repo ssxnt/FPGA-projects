c7bf0b4f0d2c0a7e70dc2c74accaeb91967154b8
Matei Dragomir <matei.a.dragomir@gmail.com>
2023-09-28 20:40:50 -0700

    skeleton of tb_statemachine

--- Lab-2/Lab-1/task5/tb_statemachine.sv
+++ Lab-2/Lab-1/task5/tb_statemachine.sv
@@ -1,8 +1,45 @@
 module tb_statemachine();
+    reg clk, rst_n;
+	reg [3:0] dscore, pscore, pcard3;
+	wire p_win, d_win;
+	wire [5:0] load;
+
+	integer pass = 0;
+	integer fail = 0;
+
+    statemachine DUT(.slow_clock(clk), .resetb(rst_n),
+                    .dscore, .pscore, .pcard3,
+                    .load_pcard1(load[0]), .load_pcard2(load[1]), .load_pcard3(load[2]),
+                    .load_dcard1(load[3]), .load_dcard2(load[4]), .load_dcard3(load[5]),
+    				.player_win_light(p_win), .dealer_win_light(d_win));
 
 // Your testbench goes here. Make sure your tests exercise the entire design
 // in the .sv file.  Note that in our tests the simulator will exit after
 // 10,000 ticks (equivalent to "initial #10000 $finish();").
+	task reset; rst_n = 1'b0; #10; rst_n = 1'b1; endtask
+
+	initial begin
+		clk = 0;
+		forever #5 clk = ~clk;
+	end
+
+    initial begin
+		#3;
+        $display("==== START FSM TEST ====");
+		$display("==== RESET ====");
+		reset;
+		#5;
+		$display("");
+		$monitor("OUT: %6b", load);
+		dscore = 4'd7;
+		pscore = 4'd7;
+		#100
+
+        $display("\n==== TEST SUMMARY ====");
+		$display("PASSED: %-d", pass);
+		$display("FAILED: %-d", fail);
+		$stop;
+    end
 						
 endmodule
 
