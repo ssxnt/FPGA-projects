a539208fa3fc7c9cb4e3338d10857801453582ad
Sant <ssants21cf@gmail.com>
2023-10-23 17:40:25 -0700

    tb task2 done

--- Lab4/CPEN311-lab4/task2/tb_rtl_task2.sv
+++ Lab4/CPEN311-lab4/task2/tb_rtl_task2.sv
@@ -1,5 +1,26 @@
 module tb_rtl_task2();
 
-// Your testbench goes here. Our toplevel will give up after 1,000,000 ticks.
+    reg clk, vgahs, vgavs, vgaclk, vgaplot;
+    reg [2:0] vgacolour;
+    reg [3:0] key;
+    reg [6:0] vgay;
+    reg [7:0] vgar, vgag, vgab, vgax;
+    reg [9:0] sw, ledr;
+
+    task2 dut(.CLOCK_50(clk), .KEY(key), .SW(sw), .LEDR(ledr), .VGA_R(vgar), .VGA_G(vgag), .VGA_B(vgab), .VGA_HS(vgahs),
+              .VGA_VS(vgavs), .VGA_CLK(vgaclk), .VGA_X(vgax), .VGA_Y(vgay), .VGA_COLOUR(vgacolour), .VGA_PLOT(vgaplot)); 
+
+    initial begin
+		clk = 0;
+		forever #5 clk = ~clk;
+	end
+
+    initial begin
+		key[3] = 1;
+	    #50;
+	    key[3] = 0;
+        #50;
+        key[3] = 1;
+	end
 
 endmodule: tb_rtl_task2
