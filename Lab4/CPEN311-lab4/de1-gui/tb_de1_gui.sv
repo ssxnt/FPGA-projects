e18d6edb31e8bdaa6920ab366851b7c7deb13e50
Matei Dragomir <matei.a.dragomir@gmail.com>
2023-10-25 22:53:37 -0700

    tb gui

--- Lab4/CPEN311-lab4/de1-gui/tb_de1_gui.sv
+++ Lab4/CPEN311-lab4/de1-gui/tb_de1_gui.sv
@@ -12,9 +12,17 @@ module tb_de1_gui();
     logic [6:0] HEX4;
     logic [6:0] HEX5;
 
+    wire [7:0] VGA_R, VGA_G, VGA_B;
+    wire [7:0] VGA_X;
+    wire [6:0] VGA_Y;
+    wire [2:0] VGA_COLOUR;
+    wire VGA_HS, VGA_VS, VGA_CLK, VGA_PLOT;
+
     de1_gui gui(.SW, .KEY, .LEDR, .HEX5, .HEX4, .HEX3, .HEX2, .HEX1, .HEX0);
 
     button_pusher dut(.CLK, .SW, .KEY, .LEDR, .HEX5, .HEX4, .HEX3, .HEX2, .HEX1, .HEX0);
+
+    vga_demo demo(.CLOCK_50(CLK), .KEY, .SW, .VGA_R, .VGA_G, .VGA_B, .VGA_HS, .VGA_VS, .VGA_CLK, .VGA_X, .VGA_Y, .VGA_COLOUR, .VGA_PLOT);
 
     initial begin
         CLK = 0;
